8a76a245877f3742498e504bd812168ab264603e
CoderMahabub <codermahabub@gmail.com>
2021-10-18 01:28:24 +0600

    Header & Footer Added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { Button, Col, Container, Row } from 'react-bootstrap';
+import footerLogo from '../../Images/viadent.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPhone, faEnvelope, faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
+    const phoneIcon = <FontAwesomeIcon icon={faPhone} />
+    const emailIcon = <FontAwesomeIcon icon={faEnvelope} />
+    const arrowRight = <FontAwesomeIcon icon={faArrowRight} />
     return (
-        <div>
-            <h1>This is Footer</h1>
+        <div className="border">
+            <Container>
+                <Row>
+                    <Col className="text-start p-3">
+                        <img className="mb-4" src={footerLogo} alt="" />
+                        <p>ViaDent is a leading dental care clinic in Bangladesh with a huge success in recent years.</p>
+                        <p> <span className="text-info">{phoneIcon} </span> House# 65, Sector 10, Dhaka</p>
+                    </Col>
+                    <Col className="d-flex flex-column mt-4">
+                        <h3>Quick Links</h3>
+                        <Link to="/home"><span className="text-info">{arrowRight} </span> Home</Link>
+                        <Link to="/home#services"><span className="text-info">{arrowRight} </span> Services</Link>
+                        <Link to="/about"><span className="text-info">{arrowRight} </span> About Us</Link>
+                        <Link to="/about"><span className="text-info">{arrowRight} </span> About Us</Link>
+                        <Link to="/contact"><span className="text-info">{arrowRight} </span> Contact Us</Link>
+                    </Col>
+                    <Col className="d-flex flex-column mt-4 text-start">
+                        <h3>Make an Appointment</h3>
+                        <p>You can/mail call us 24/7. We are glad to receive your appointment.</p>
+                        <p> <span className="text-info">{emailIcon} </span> support@viadent.com</p>
+                        <Button variant="info">+88017000000</Button>
+                    </Col>
+                </Row>
+            </Container>
+            <p className="bg-dark text-light py-2 mb-0">Copyright © 2021. All Right Reserved || Developed by <a className="text-info" href="https://codermahabub.com">CoderMahabub</a></p>
         </div>
     );
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import logo from "../../Images/viadent.png";
+import { HashLink } from 'react-router-hash-link';
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is Header</h1>
-        </div>
+        <>
+            <Navbar collapseOnSelect expand="lg" sticky="top" className="border border-secondary">
+                <Container>
+                    <Navbar.Brand as={HashLink} to="/home">
+                        <img
+                            src={logo}
+                            width="90%"
+                            height="auto"
+                            className="d-inline-block align-top"
+                            alt="logo"
+                        />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+                        <Nav.Link as={HashLink} to="/home">Home</Nav.Link>
+                        <Nav.Link as={HashLink} to="/home/#services">services</Nav.Link>
+                        <Navbar.Text>
+                            Signed in as: <a as={HashLink} to="#login"> Mark Otto</a>
+                        </Navbar.Text>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
     );
 };
 
