6efd3bf42cf0e6ade2f6ccfefa9d400bcf11fed5
CoderMahabub <codermahabub@gmail.com>
2021-10-18 02:17:49 +0600

    Banner Added

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import './Banner.css';
+import { Button, Carousel } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import banner1 from '../../Images/banner/banner1.jpg';
+import banner2 from '../../Images/banner/banner2.jpg';
+import banner3 from '../../Images/banner/banner3.jpg';
+
+const Banner = () => {
+    return (
+        <Carousel>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={banner1}
+                    alt="First slide"
+                />
+                <Carousel.Caption className="mb-4">
+                    <h3 className="text-primary fw-bold fs-2">Quality Services</h3>
+                    <p className="text-primary fw-bold">We offer quality services instead of focusing on quantity</p>
+                    <Button variant="primary">
+                        <Link className="text-decoration-none text-light" to="/home#services">Our Services</Link>
+                    </Button>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={banner2}
+                    alt="Second slide"
+                />
+
+                <Carousel.Caption className="mb-4">
+                    <h3 className="text-primary fw-bold fs-2">Contact Us</h3>
+                    <p className="text-primary fw-bold">If you need any info, please contact us now</p>
+                    <Button variant="primary">
+                        <Link className="text-decoration-none text-light" to="/contact"> Contact Us</Link>
+                    </Button>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={banner3}
+                    alt="Third slide"
+                />
+
+                <Carousel.Caption className="mb-4">
+                    <h3 className="text-primary fw-bold fs-2">About Us</h3>
+                    <p className="text-primary fw-bold">To know more in detail, don't forget to contact us.</p>
+                    <Button variant="primary">
+                        <Link className="text-decoration-none text-light" to="/home#services"> About Us</Link>
+                    </Button>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+    );
+};
+
+export default Banner;
