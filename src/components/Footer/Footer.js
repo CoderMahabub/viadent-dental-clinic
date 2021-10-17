db37ea06e6c39907577c538c47b1538b53402850
CoderMahabub <codermahabub@gmail.com>
2021-10-18 02:24:09 +0600

    Footer Retouched

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,29 +12,29 @@ const Footer = () => {
     return (
         <div className="border">
             <Container>
-                <Row>
+                <Row className="py-3">
                     <Col className="text-start p-3">
                         <img className="mb-4" src={footerLogo} alt="" />
                         <p>ViaDent is a leading dental care clinic in Bangladesh with a huge success in recent years.</p>
-                        <p> <span className="text-info">{phoneIcon} </span> House# 65, Sector 10, Dhaka</p>
+                        <p> <span className="text-primary">{phoneIcon} </span> House# 65, Sector 10, Dhaka</p>
                     </Col>
                     <Col className="d-flex flex-column mt-4 text-decoration-none">
-                        <h3>Quick Links</h3>
-                        <Link className="text-decoration-none text-dark" to="/home"><span className="text-info">{arrowRight} </span> Home</Link>
-                        <Link className="text-decoration-none text-dark" to="/home#services"><span className="text-info">{arrowRight} </span> Services</Link>
-                        <Link className="text-decoration-none text-dark" to="/about"><span className="text-info">{arrowRight} </span> About Us</Link>
-                        <Link className="text-decoration-none text-dark" to="/about"><span className="text-info">{arrowRight} </span> About Us</Link>
-                        <Link className="text-decoration-none text-dark" to="/contact"><span className="text-info">{arrowRight} </span> Contact Us</Link>
+                        <h3 className="fw-bold"><u>Quick Links</u></h3>
+                        <Link className="text-decoration-none text-dark" to="/home"><span className="text-primary">{arrowRight} </span> Home</Link>
+                        <Link className="text-decoration-none text-dark" to="/home#services"><span className="text-primary">{arrowRight} </span> Services</Link>
+                        <Link className="text-decoration-none text-dark" to="/about"><span className="text-primary">{arrowRight} </span> About Us</Link>
+                        <Link className="text-decoration-none text-dark" to="/about"><span className="text-primary">{arrowRight} </span> About Us</Link>
+                        <Link className="text-decoration-none text-dark" to="/contact"><span className="text-primary">{arrowRight} </span> Contact Us</Link>
                     </Col>
                     <Col className="d-flex flex-column mt-4 text-start">
-                        <h3>Make an Appointment</h3>
+                        <h3 className="fw-bold"><u>Make an Appointment</u></h3>
                         <p>You can/mail call us 24/7. We are glad to receive your appointment.</p>
-                        <p> <span className="text-info">{emailIcon} </span> support@viadent.com</p>
-                        <Button className="text-light fw-bold fs-5" variant="info">+880 0170 00000</Button>
+                        <p> <span className="text-primary">{emailIcon} </span> support@viadent.com</p>
+                        <Button className="text-light fw-bold fs-5" variant="primary">+880 0170 00000</Button>
                     </Col>
                 </Row>
             </Container>
-            <p className="bg-dark text-light py-2 mb-0">Copyright © 2021. All Right Reserved || Developed by <a className="text-info" href="https://codermahabub.com">CoderMahabub</a></p>
+            <p className="bg-dark text-light py-2 mb-0">Copyright © 2021. All Right Reserved || Developed by <a className="text-primary fs-5" href="https://codermahabub.com">CoderMahabub</a></p>
         </div>
     );
 };
