b463d325ded627b5b5925e8f33f7a681cf260f83
CoderMahabub <codermahabub@gmail.com>
2021-10-18 01:32:40 +0600

    Footer Update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -18,19 +18,19 @@ const Footer = () => {
                         <p>ViaDent is a leading dental care clinic in Bangladesh with a huge success in recent years.</p>
                         <p> <span className="text-info">{phoneIcon} </span> House# 65, Sector 10, Dhaka</p>
                     </Col>
-                    <Col className="d-flex flex-column mt-4">
+                    <Col className="d-flex flex-column mt-4 text-decoration-none">
                         <h3>Quick Links</h3>
-                        <Link to="/home"><span className="text-info">{arrowRight} </span> Home</Link>
-                        <Link to="/home#services"><span className="text-info">{arrowRight} </span> Services</Link>
-                        <Link to="/about"><span className="text-info">{arrowRight} </span> About Us</Link>
-                        <Link to="/about"><span className="text-info">{arrowRight} </span> About Us</Link>
-                        <Link to="/contact"><span className="text-info">{arrowRight} </span> Contact Us</Link>
+                        <Link className="text-decoration-none text-dark" to="/home"><span className="text-info">{arrowRight} </span> Home</Link>
+                        <Link className="text-decoration-none text-dark" to="/home#services"><span className="text-info">{arrowRight} </span> Services</Link>
+                        <Link className="text-decoration-none text-dark" to="/about"><span className="text-info">{arrowRight} </span> About Us</Link>
+                        <Link className="text-decoration-none text-dark" to="/about"><span className="text-info">{arrowRight} </span> About Us</Link>
+                        <Link className="text-decoration-none text-dark" to="/contact"><span className="text-info">{arrowRight} </span> Contact Us</Link>
                     </Col>
                     <Col className="d-flex flex-column mt-4 text-start">
                         <h3>Make an Appointment</h3>
                         <p>You can/mail call us 24/7. We are glad to receive your appointment.</p>
                         <p> <span className="text-info">{emailIcon} </span> support@viadent.com</p>
-                        <Button variant="info">+88017000000</Button>
+                        <Button className="text-light fw-bold fs-5" variant="info">+880 0170 00000</Button>
                     </Col>
                 </Row>
             </Container>
